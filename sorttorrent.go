1ef2b0ebe369e58883682cb29d6c9c6ae39ee4b6
LazyHacker <github@lazyhacker.com>
2020-08-09 01:18:21 -0700

    change to name of the file to have the episode number first in the name.

--- sorttorrent.go
+++ sorttorrent.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -23,6 +24,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to open dir. %v\n", err)
 	}
+	defer f.Close()
 
 	files, err := f.Readdir(0)
 	if err != nil {
@@ -36,18 +38,22 @@ func main() {
 
 			// Use parser to look at the file name and extract the title, episode #, etc.
 			tinfo, err := parsetorrentname.Parse(file.Name())
+			//pretty print parsed torrent info in JSON format
+			//j, _ := json.MarshalIndent(tinfo, "", "\t")
+			//fmt.Printf("%v\n", string(j))
 			if err != nil {
 				log.Printf("Unable to parse file name, %v.  %v\n", file.Name(), err)
 				continue
 			}
 
+			// Use series title for the directory name.
 			sdir := filepath.Join(*dir, tinfo.Title)
 			err = os.Mkdir(sdir, 0755)
 			if err != nil {
 				log.Printf("Error message when creating directory. %v", err)
 			}
 			oldpath := filepath.Join(*dir, file.Name())
-			newpath := filepath.Join(sdir, file.Name())
+			newpath := filepath.Join(sdir, convertName(tinfo))
 
 			if _, err := os.Stat(newpath); err == nil {
 				log.Printf("%v already exists! Skip moving.\n", newpath)
@@ -61,4 +67,14 @@ func main() {
 			}
 		}
 	}
+}
+
+// convertName will change the name of the file to have episode number first in
+// order to allow proper sorting in case the series episodes are from different
+// groups.
+func convertName(n *parsetorrentname.TorrentInfo) string {
+	//j, _ := json.MarshalIndent(tinfo, "", "\t")
+	//fmt.Printf("%v\n", string(j))
+	return fmt.Sprintf("%d - %v [%v] [%v].%v", n.Episode, n.Title, n.Website, n.Resolution, n.Container)
+
 }
